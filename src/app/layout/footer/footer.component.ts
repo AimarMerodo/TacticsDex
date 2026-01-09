6579cde5c226389de0a67178ed71ddd1773b4b2c
Aimar <aimarmerodoa@gmail.com>
2026-01-09 17:48:24 +0100

    Cambios en el estilo del footer y link a la PokeApi

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
-  year = new Date().getFullYear()
+  year = new Date().getFullYear();
+
+  pagesList = [
+    { title: "Home", route: "/" },
+    { title: "Pokedex", route: "/pokedex" },
+    { title: "Builder", route: "/builder" }
+  ];
+
 }
